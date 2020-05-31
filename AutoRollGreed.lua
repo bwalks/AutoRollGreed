6103e8ae54b2c7a14cd73f4d4d89d6c4c99847f2
Brendan Walker <bwalks@users.noreply.github.com>
2020-05-31 15:50:13 -0400

    Add files via upload

---
+++ AutoRollGreed.lua
@@ -0,0 +1,49 @@
+local addonName, addon = ...
+
+SLASH_AUTOROLLGREED1 = "/autorollgreed"
+SLASH_AUTOROLLGREED2 = "/arg"
+
+SlashCmdList["AUTOROLLGREED"] = function(msg, editBox)
+  if msg == 'enable' then
+    autoRollGreedEnabled = true
+    print("AutoRollGreed is enabled")
+  elseif msg == 'disable' then
+    autoRollGreedEnabled = false
+    print("AutoRollGreed is disabled")
+  elseif msg == 'status' then
+    if autoRollGreedEnabled then
+        print("AutoRollGreed is enabled")
+    else
+        print("AutoRollGreed is disabled")
+    end
+  else
+    print("AutoRollGreed can be enabled/disabled by doing /arg or /autorollgreed")
+    print("/arg enable -- enables AutoRollGreed")
+    print("/arg disable -- disables AutoRollGreed")
+    print("/arg status -- prints if it is currently enabled/disabled")
+  end
+end
+
+
+local function onEvent(self, event, arg1, ...)
+    if(event == "ADDON_LOADED") then
+        -- This event is fired off for all addons. Ignore everything else
+        if  arg1 ~= "AutoRollGreed" then
+          return
+        end
+        if autoRollGreedEnabled == nil then
+            autoRollGreedEnabled = true
+        end
+    else
+        texture, name, count, quality, bindOnPickUp, canNeed, canGreed = GetLootRollItemInfo(arg1)
+        if(quality <= 2 and canGreed) then
+            RollOnLoot(arg1, 2)
+        end
+    end
+end
+
+addon.core = {};
+addon.core.frame = CreateFrame("Frame");
+addon.core.frame:SetScript("OnEvent", onEvent);
+addon.core.frame:RegisterEvent("START_LOOT_ROLL")
+addon.core.frame:RegisterEvent("ADDON_LOADED");
